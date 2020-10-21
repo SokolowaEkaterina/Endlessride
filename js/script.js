5306ec501b1269535eb42991856ed41f417199cf
SokolowaEkaterina <soko20wa.ek0ter1na@gmail.com>
2020-10-21 21:23:42 +0400

    scroll-inimait

--- js/script.js
+++ js/script.js
@@ -5,4 +5,23 @@ $(document).ready(function(){
         $('#mobile-nav').slideToggle();
     });
 
+    AOS.init({
+        disable: false,
+        startEvent: 'DOMContentLoaded',
+        initClassName: 'aos-init',
+        animatedClassName: 'aos-animate',
+        useClassNames: false,
+        disableMutationObserver: false,
+        debounceDelay: 50,
+        throttleDelay: 99,
+        
+        offset: 100,
+        delay: 0,
+        duration: 500,
+        easing: 'ease',
+        once: true,
+        mirror: false,
+        anchorPlacement: 'top-bottom',
+      });
+
 });
